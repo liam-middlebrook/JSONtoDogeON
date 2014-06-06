805fec765b8fbd2201e1aaa87437c01dce371787
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-06-05 23:03:04 -0400

    Added sample4.JSON Added support for parsing around quotes

--- dogeON.py
+++ dogeON.py
@@ -13,7 +13,10 @@ with open (jsonFile, "r") as myfile:
 inQuotes = False
 
 def parse(jsonString):
+    inQuotes = False
     for i, char in enumerate(jsonString):
+        if char == '\"':
+            inQuotes = not inQuotes
         if not inQuotes:
             if char == '{':
                 jsonString = jsonString[:i] + " such "+ jsonString[1+i:]
